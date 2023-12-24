fa8569de659157cd77ce560e653a2883b3982945
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-12-24 15:47:12 +0530

    updated requirefield

--- src/Routes/Api.js
+++ src/Routes/Api.js
@@ -9,13 +9,32 @@ const {
   update: updateBlog,
 } = require('../Controllers/blogController');
 
-
+const {
+  create: createBlogDetails,
+  read: readBlogDetails,
+  delete: deleteBlogDetails,
+  update: updateBlogDetails,
+} = require('../Controllers/blogDetailsController');
 // Define routes
 router.get('/', createBlog);
 router.get('/blog/read', readBlog);
 router.get('/blog/delete', deleteBlog);
 router.get('/blog/update', updateBlog);
 
-// Repeat the above for other controllers and functions
+router.get('/', createBlogDetails);
+router.get('/blog/read', readBlogDetails);
+router.get('/blog/delete', deleteBlogDetails);
+router.get('/blog/update', updateBlogDetails);
 
+const {
+  create: createComment,
+  read: readComment,
+  delete: deleteComment,
+  update: updateComment,
+} = require('../Controllers/commentController');
+
+router.get('/', createComment);
+router.get('/blog/read', readComment);
+router.get('/blog/delete', deleteComment);
+router.get('/blog/update', updateComment);
 module.exports = router;
